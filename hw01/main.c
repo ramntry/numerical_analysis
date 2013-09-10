3aed77520424696caac8703b7a186d69963b5011
ramntry <ramntry@gmail.com>
2013-09-10 21:30:12 +0400

    init and start hw01

---
+++ hw01/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct
+{
+    size_t deg;
+    double *coeffs;
+}
+Polynomial;
+
+void polynomialInit(Polynomial *polynomial)
+{
+}
+
+void polynomialDispose(Polynomial *polynomial)
+{
+}
+
+int main()
+{
+    return 0;
+}
