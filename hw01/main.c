41c0a0c80da20c89bbf530b3b797d7b54d834be2
ramntry <ramntry@gmail.com>
2013-09-10 22:33:16 +0400

    polynomials primitives was added

--- hw01/main.c
+++ hw01/main.c
@@ -1,21 +1,98 @@
+#include <assert.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+#define DISPOSED_POLYNOMIAL_DEG (-1)
+#define READ_POLYNOMIAL_ERROR (-1)
+#define READ_POLYNOMIAL_OK (1)
+#define PRINT_POLYNOMIAL_EPS (1.e-9)
+#define PRINT_POLYNOMIAL_PRECISION (2)
 
 typedef struct
 {
-    size_t deg;
+    int deg;
     double *coeffs;
 }
 Polynomial;
 
-void polynomialInit(Polynomial *polynomial)
+void initPolynomial(Polynomial *polynomial, int deg)
 {
+    assert(deg >= DISPOSED_POLYNOMIAL_DEG);
+    polynomial->deg = deg;
+    if (deg == DISPOSED_POLYNOMIAL_DEG) {
+        polynomial->coeffs = NULL;
+        return;
+    }
+    polynomial->coeffs = (double *)malloc(sizeof(double) * (deg + 1));
 }
 
-void polynomialDispose(Polynomial *polynomial)
+void disposePolynomial(Polynomial *polynomial)
 {
+    free(polynomial->coeffs);
+    polynomial->coeffs = NULL;
+    polynomial->deg = DISPOSED_POLYNOMIAL_DEG;
+}
+
+int readPolynomial(Polynomial *polynomial, FILE *fd)
+{
+    if (fscanf(fd, "%d", &polynomial->deg) != 1) {
+        polynomial->deg = DISPOSED_POLYNOMIAL_DEG;
+        return READ_POLYNOMIAL_ERROR;
+    }
+    initPolynomial(polynomial, polynomial->deg);
+    for (int i = polynomial->deg; i >= 0; --i) {
+        if (fscanf(fd, "%lf", &polynomial->coeffs[i]) != 1) {
+            disposePolynomial(polynomial);
+            return READ_POLYNOMIAL_ERROR;
+        }
+    }
+    return READ_POLYNOMIAL_OK;
+}
+
+void printPolynomial(Polynomial const *polynomial, FILE *fd)
+{
+    assert(polynomial->deg != DISPOSED_POLYNOMIAL_DEG);
+    for (int i = polynomial->deg; i >= 0; --i) {
+        if (i == polynomial->deg) {
+            fprintf(fd, "%.*lf", PRINT_POLYNOMIAL_PRECISION, polynomial->coeffs[i]);
+        } else if (fabs(polynomial->coeffs[i]) >= PRINT_POLYNOMIAL_EPS) {
+            if (polynomial->coeffs[i] >= 0) {
+                fprintf(fd, " + ");
+            } else {
+                fprintf(fd, " - ");
+            }
+            if (fabs(polynomial->coeffs[i] - 1.) >= PRINT_POLYNOMIAL_EPS) {
+                fprintf(fd, "%.*lf", PRINT_POLYNOMIAL_PRECISION, fabs(polynomial->coeffs[i]));
+            }
+        } else {
+            continue;
+        }
+        if (i >= 1) {
+            fprintf(fd, "x");
+        }
+        if (i > 1) {
+            fprintf(fd, "^%d", i);
+        }
+    }
+}
+
+Polynomial getPolynomialFromUser()
+{
+    printf("Enter polynomial's degree and coefficients separated by spaces: ");
+    Polynomial polynomial;
+    if (readPolynomial(&polynomial, stdin) != READ_POLYNOMIAL_OK) {
+        fprintf(stderr, "Polynomial reading error!\n");
+        exit(EXIT_FAILURE);
+    }
+    return polynomial;
 }
 
 int main()
 {
+    Polynomial p = getPolynomialFromUser();
+    printPolynomial(&p, stdout);
+    putchar('\n');
+    disposePolynomial(&p);
     return 0;
 }
