593921a772edbd9a7591f42c754d91e411fdaaad
ramntry <ramntry@gmail.com>
2014-03-04 23:35:10 +0400

    hw10 is done

--- hw10/main.c
+++ hw10/main.c
@@ -5,8 +5,6 @@
 #define SIZE 5
 #define DOUBLE_WIDTH 8
 
-double const eps = 1.0e-5;
-
 // 27
 complex double matrix[SIZE][SIZE] = {
   {  1,  3, -2,  0, -2 },
@@ -26,6 +24,10 @@ complex double vector[SIZE] = {
   7.5,
   3.3
 };
+
+complex double vector_y[SIZE];
+complex double vector_x[SIZE];
+complex double control_vector[SIZE];
 
 void decompose() {
   for (int i = 0; i < SIZE; ++i)
@@ -47,20 +49,80 @@ void calc_control() {
         control[i][j] += decomposed[k][i] * decomposed[k][j];
 }
 
+void print_complex(complex double z) {
+  printf(" % -4.1f%+.2fi", creal(z), cimag(z));
+}
+
 void print_matrix(complex double m[SIZE][SIZE]) {
   for (int i = 0; i < SIZE; ++i) {
     for (int j = 0; j < SIZE; ++j)
-      printf(" % -4.1f%+.2fi", creal(m[i][j]), cimag(m[i][j]));
+      print_complex(m[i][j]);
     printf("\n");
   }
   printf("\n");
 }
 
+void print_vector(complex double v[SIZE]) {
+  for (int i = 0; i < SIZE; ++i) {
+    print_complex(v[i]);
+    printf("\n");
+  }
+  printf("\n");
+}
+
+void back_pass_y()
+{
+  for (int i = 0; i < SIZE; ++i) {
+    vector_y[i] = vector[i];
+    for (int j = 0; j < i; ++j)
+      vector_y[i] -= decomposed[j][i] * vector_y[j];
+    vector_y[i] /= decomposed[i][i];
+  }
+}
+
+void back_pass_x()
+{
+  for (int i = SIZE - 1; i >= 0; --i) {
+    vector_x[i] = vector_y[i];
+    for (int j = i + 1; j < SIZE; ++j)
+      vector_x[i] -= decomposed[i][j] * vector_x[j];
+    vector_x[i] /= decomposed[i][i];
+  }
+}
+
+void recalc_vector()
+{
+  for (int i = 0; i < SIZE; ++i) {
+    for (int j = 0; j < SIZE; ++j)
+      control_vector[i] += vector_x[j] * matrix[i][j];
+  }
+}
+
 int main() {
+  printf("A =\n");
   print_matrix(matrix);
+  printf("b = \n");
+  print_vector(vector);
+
   decompose();
+  printf("S =\n");
   print_matrix(decomposed);
+
   calc_control();
+  printf("S'S =\n");
   print_matrix(control);
+
+  back_pass_y();
+  printf("y (where S'y = b, y = Sx) = \n");
+  print_vector(vector_y);
+
+  back_pass_x();
+  printf("x = \n");
+  print_vector(vector_x);
+
+  recalc_vector();
+  printf("Ax = \n");
+  print_vector(control_vector);
   return 0;
 }
+
